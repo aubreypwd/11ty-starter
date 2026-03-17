2cc3d9197bcd8bc7a13762e8642b116212193753
Aubrey Portwood <code@aubreypwd.com>
2026-03-17 14:57:03 -0600

    We no longer override this way.
    Everything is controlled by site.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,5 @@ require( 'module-alias/register' );
 
 module.exports = async function ( eleventyConfig ) {
 
-	return require( './src/_includes/11ty-starter-common/.eleventy.js' )( eleventyConfig,
-
-		// Flags.
-		{},
-
-		// Overrides.
-		{}
-	);
+	return require( './src/_includes/11ty-starter-common/.eleventy.js' )( eleventyConfig );
 };
